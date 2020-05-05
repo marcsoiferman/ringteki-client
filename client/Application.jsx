35f434d67dd1124199e050e95be86539a09567f9
Marc Soiferman <t>
2020-05-05 18:46:11 -0500

    test

--- client/Application.jsx
+++ client/Application.jsx
@@ -151,7 +151,7 @@ class App extends React.Component {
                 reconnectionDelay: 1000,
                 reconnectionDelayMax: 5000,
                 reconnectionAttempts: 5,
-                query: this.props.token ? 'token=' + this.props.token : undefined
+                query: 'transport=polling' + this.props.token ? '&token=' + this.props.token : ''
             });
 
             gameSocket.on('connect_error', (err) => {
